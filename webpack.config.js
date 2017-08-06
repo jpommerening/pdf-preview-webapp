1cac381e02278665fb8f662bb6d6a2d3b977d93c
Jonas Pommerening <jonas.pommerening@gmail.com>
2017-08-06 19:14:38 +0200

    Add (experimental) hot reload support

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const DEV_ENTRIES_PATTERN = /(^|\/)(webpack-dev-server\/client|webpack\/hot)(\/|$)/;
+
 module.exports = [
    require( './webpack.browser.config' ),
    require( './webpack.server.config' )
@@ -6,15 +9,28 @@ module.exports = [
 module.exports[ 0 ].devServer = require( './webpack-dev-server.config.js' );
 module.exports[ 0 ].plugins.push( {
    apply: compiler => {
+      compiler.plugin( 'entry-option', ( context, entry ) => {
+         if(
+            entry.browser && Array.isArray( entry.browser ) &&
+            entry.browser.some( entry => DEV_ENTRIES_PATTERN.test( entry ) )
+         ) {
+            entry.browser.unshift( 'react-hot-loader/patch' );
+            module.exports[ 0 ].plugins.forEach( p => {
+               if( p instanceof ExtractTextPlugin ) {
+                  console.log( 'disable etp' );
+                  p.options.disable = true;
+               }
+            } );
+         }
+      } );
    }
 } );
 module.exports[ 1 ].plugins.push( {
    apply: compiler => {
       // remove webpack dev server injections from server bundle
-      const excludePattern = /(^|\/)(webpack-dev-server\/client|webpack\/hot)(\/|$)/;
       compiler.plugin( 'entry-option', ( context, entry ) => {
          if( entry.server && Array.isArray( entry.server ) ) {
-            entry.server = entry.server.filter( entry => !excludePattern.test( entry ) );
+            entry.server = entry.server.filter( entry => !DEV_ENTRIES_PATTERN.test( entry ) );
          }
       } );
    }
