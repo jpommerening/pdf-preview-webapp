64df0934961e3413eb499e361e878567d94539f3
Jonas Pommerening <jonas.pommerening@gmail.com>
2017-08-07 11:44:51 +0200

    API fixes and improvements

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ module.exports = function( options ) {
    return middleware( Object.assign( {
       contentBase: __dirname,
       scripts: [ 'dist/browser.js' ],
-      styles: [ 'dist/style.css' ]
+      styles: [ 'dist/style.css' ],
+      assets: [
+         'dist/browser.js.map',
+         'dist/style.css.map',
+         'dist/66d3c7fee01f3bc37029.worker.js',
+         'dist/66d3c7fee01f3bc37029.worker.js.map'
+      ]
    }, options ) );
 };
